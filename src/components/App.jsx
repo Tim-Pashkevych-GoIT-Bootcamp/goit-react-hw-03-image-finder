9009a84c63c5f2423959b53ca0c0d87ce2d045e8
Tymofii Pashkevych <tymofii.pashkevych@gmail.com>
2024-01-11 22:47:44 +0200

    Refactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,39 +1,85 @@
 import React, { Component } from 'react';
-import { ToastContainer } from 'react-toastify';
+import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { Searchbar, ImageGallery, Loader, Modal } from 'components';
+import * as API from '../utils/apiPexels';
+import { Button, Searchbar, ImageGallery, Loader, Modal } from 'components';
 
 const INITIAL_STATE = {
   keyword: '',
+  page: 1,
+  total: 0,
+  images: [],
   showLoader: false,
 };
 
 export class App extends Component {
   state = { ...INITIAL_STATE };
 
-  shouldComponentUpdate(prevProps, prevState) {
-    const { keyword, showLoader } = this.state;
-    // Prevent fetching the same images if the keyword wasn't changed
-    return keyword !== prevState.keyword || showLoader !== prevState.showLoader;
+  async componentDidUpdate(prevProps, prevState) {
+    if (
+      prevState.keyword !== this.state.keyword ||
+      prevState.page !== this.state.page
+    ) {
+      const { images, total } = await this.getImages();
+      this.setState(prevState => ({
+        images: [...prevState.images, ...images],
+        total,
+      }));
+    }
   }
 
+  getImages = async () => {
+    let response = { images: [], total: 0 };
+    const { page, keyword } = this.state;
+
+    try {
+      // Show loader
+      this.toggleLoader();
+      // Fetch data
+      response = await API.getImages(keyword, page);
+    } catch (error) {
+      // Show error
+      toast.error(error.message);
+    } finally {
+      // Hide loader
+      this.toggleLoader();
+    }
+
+    return response;
+  };
+
   updateKeyword = keyword => {
-    this.setState({ keyword });
+    this.setState({ ...INITIAL_STATE, keyword });
   };
 
   toggleLoader = () => {
     this.setState(prevState => ({ showLoader: !prevState.showLoader }));
   };
 
+  loadMoreClick = () => {
+    this.setState(prevState => ({ page: prevState.page + 1 }));
+  };
+
   render() {
+    const { images, total, keyword } = this.state;
+
     return (
       <>
         <Searchbar onSubmit={this.updateKeyword} />
 
-        <ImageGallery
-          keyword={this.state.keyword}
-          toggleLoader={this.toggleLoader}
-        />
+        {images.length > 0 && (
+          <ImageGallery
+            keyword={keyword}
+            images={images}
+            toggleLoader={this.toggleLoader}
+          />
+        )}
+
+        {images.length < total && (
+          <Button type="button" onClick={this.loadMoreClick}>
+            Load more
+          </Button>
+        )}
 
         {this.state.showLoader && (
           <Modal>
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,56 +1,11 @@
 import React, { Component } from 'react';
-import { toast } from 'react-toastify';
 import css from './ImageGallery.module.css';
-import * as API from '../../utils/apiPexels';
-import { Button, Image, Modal } from 'components';
-import { ImageGalleryList } from 'components/ImageGalleryList/ImageGalleryList';
+import { Image, ImageGalleryList, Modal } from 'components';
 
-const INITIAL_STATE = { page: 0, total: 0, selectedImageId: 0, images: [] };
+const INITIAL_STATE = { selectedImageId: 0 };
 
 export class ImageGallery extends Component {
   state = { ...INITIAL_STATE };
-
-  async componentDidUpdate(prevProps, prevState) {
-    const { page } = this.state;
-    const { keyword } = this.props;
-
-    if (prevProps.keyword !== keyword) {
-      // Reset the state
-      await this.setState({ ...INITIAL_STATE });
-      // Request a data for a first page
-      this.setState({ page: 1 });
-    }
-
-    // Fetch new images
-    if (page && prevState.page !== page) {
-      const { images, total } = await this.getImages();
-      this.setState(prevState => ({
-        images: [...prevState.images, ...images],
-        total,
-      }));
-    }
-  }
-
-  getImages = async () => {
-    let response = { images: [], total: 0 };
-    const { page } = this.state;
-    const { keyword, toggleLoader } = this.props;
-
-    try {
-      // Show loader
-      toggleLoader();
-      // Fetch data
-      response = await API.getImages(keyword, page);
-    } catch (error) {
-      // Show error
-      toast.error(error.message);
-    } finally {
-      // Hide loader
-      toggleLoader();
-    }
-
-    return response;
-  };
 
   showModal = selectedImageId => {
     this.setState({ selectedImageId });
@@ -60,36 +15,23 @@ export class ImageGallery extends Component {
     this.setState({ selectedImageId: 0 });
   };
 
-  loadMoreClick = () => {
-    this.setState(prevState => ({ page: prevState.page + 1 }));
-  };
-
   render() {
-    const { images, total, selectedImageId } = this.state;
+    const { images } = this.props;
+    const { selectedImageId } = this.state;
     const selectedImage = images.find(image => image.id === selectedImageId);
 
     return (
       <div className={css.galleryContainer}>
-        {images.length > 0 && (
-          <>
-            <ImageGalleryList images={images} showModal={this.showModal} />
+        <ImageGalleryList images={images} showModal={this.showModal} />
 
-            {images.length < total && (
-              <Button type="button" onClick={this.loadMoreClick}>
-                Load more
-              </Button>
-            )}
-
-            {!!selectedImageId && (
-              <Modal onClose={this.hideModal}>
-                <Image
-                  showLoader="true"
-                  src={selectedImage.src.original}
-                  alt={selectedImage.alt}
-                />
-              </Modal>
-            )}
-          </>
+        {!!selectedImageId && (
+          <Modal onClose={this.hideModal}>
+            <Image
+              showLoader="true"
+              src={selectedImage.src.original}
+              alt={selectedImage.alt}
+            />
+          </Modal>
         )}
       </div>
     );
